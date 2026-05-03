9536ff8eecf26b4ddb6686413d66ce96cef1079a
BoCoder17 <bagdankloun@gmail.com>
2026-05-03 08:48:27 +0600

    chore: setup deployment scripts and base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/balalaika-hero/',
+  plugins: [],
   publicDir: 'public',
   server: {
     port: 3000,
